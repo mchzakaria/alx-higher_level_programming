6964bf709de08190220e4eab2ff6417fd037a831
mchzakaria <achrafelghaiti4@gmail.com>
2023-10-24 04:40:25 -0700

    fin2

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,7 @@
-[200~#!/usr/bin/python3
-        """A module for square"""
+#!/usr/bin/python3
+"""Square class defination"""
 
 
-        class Square:
-            """Represent a square."""
-                pass
+class Square:
+    """Square class body"""
+    pass
