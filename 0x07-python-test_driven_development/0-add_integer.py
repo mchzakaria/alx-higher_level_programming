5f44ea230c2117aacdff6728c1efe9d00e93e437
mchzakaria <achrafelghaiti4@gmail.com>
2023-10-29 07:06:01 -0700

    fin

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""0-add_integer
+The function "add_integer"  returns the sum of two integers.
+"""
+def add_integer(a, b=98):
+    """adds two integers function body"""
+
+    if type(a) not in [int, float]:
+        raise TypeError("a must be an integer")
+    if type(b) not in [int, float]:
+        raise TypeError("b must be an integer")
+
+    return int(a) + int(b)
